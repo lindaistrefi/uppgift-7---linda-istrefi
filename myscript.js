199fdf16d3fcb8d70f67914589d916cfd95c8b8a
Linda Istrefi <linda.istrefi@lerniastudent.se>
2023-10-06 17:13:06 +0200

    first commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+const typed = new Typed(".text", {
+    strings: ["Student", "Future", "System", "Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+
